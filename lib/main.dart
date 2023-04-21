5bcd0286c39a0815e9a2047624bfc06032718505
hyunjin1012 <hyunjin1012@gmail.com>
2023-04-21 10:33:57 -0700

    set theme and add comps

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch(
+                backgroundColor: const Color(0xFFE7626C)),
+            textTheme: const TextTheme(
+                displayLarge: TextStyle(color: Color(0xFF232B55))),
+            cardColor: const Color(0xFFF4EDDB)),
+        home: const HomeScreen());
   }
 }
